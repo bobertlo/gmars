171d067020967746f893b2bf5e7c2bc846e67743
Robert Lowry <bobertlo@gmail.com>
2024-10-27 17:46:10 -0500

    finish 88 load file parser

--- pkg/mars/load_test.go
+++ pkg/mars/load_test.go
@@ -13,7 +13,7 @@ const imp88 = `;redcode
 ;strategy this is the simplest program
 ;strategy it was described in the initial articles
 
-		MOV # 0, $ 1
+		MOV $ 0, $ 1
 		END 0
 `
 
@@ -26,4 +26,30 @@ func TestLoadImp(t *testing.T) {
 	require.Equal(t, "Imp", data.Name)
 	require.Equal(t, "A K Dewdney", data.Author)
 	require.Equal(t, "this is the simplest program\nit was described in the initial articles\n", data.Strategy)
+	require.Equal(t, 0, data.Start)
+	require.Equal(t, 1, len(data.Code))
+	require.Equal(t, Instruction{Op: MOV, OpMode: I, AMode: DIRECT, A: 0, BMode: DIRECT, B: 1}, data.Code[0])
+}
+
+func TestLoadDwarf(t *testing.T) {
+	config := StandardConfig()
+
+	dwarf_code := `
+	ADD #  4,  $  3
+	MOV $  2,  @  2
+	JMP $ -2,  $  0
+	DAT #  0,  #  0
+	`
+
+	reader := strings.NewReader(dwarf_code)
+	data, err := ParseLoadFile(reader, config)
+	require.NoError(t, err)
+	require.Equal(t, 0, data.Start)
+	require.Equal(t, 4, len(data.Code))
+	require.Equal(t, []Instruction{
+		{Op: ADD, OpMode: AB, AMode: IMMEDIATE, A: 4, BMode: DIRECT, B: 3},
+		{Op: MOV, OpMode: I, AMode: DIRECT, A: 2, BMode: B_INDIRECT, B: 2},
+		{Op: JMP, OpMode: B, AMode: DIRECT, A: 8000 - 2, BMode: DIRECT, B: 0},
+		{Op: DAT, OpMode: F, AMode: IMMEDIATE, A: 0, BMode: IMMEDIATE, B: 0},
+	}, data.Code)
 }
