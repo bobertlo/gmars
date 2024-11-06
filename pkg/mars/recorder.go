0ccf9da80d367afc8bb43eb503dd876ee90e754c
Robert Lowry <bobertlo@gmail.com>
2024-11-05 20:55:57 -0600

    add Recorder interface

---
+++ pkg/mars/recorder.go
@@ -0,0 +1,102 @@
+package mars
+
+import "fmt"
+
+type CoreState uint8
+
+const (
+	CoreEmpty CoreState = iota
+	CoreDataWritten
+	CoreDataExecuted
+	CoreCodeWritten
+	CoreCodeExecuted
+	CoreRead
+	CoreIncremented
+	CoreDecremented
+)
+
+type Recorder interface {
+	Reporter
+
+	// GetMemState takes a memory address and returns the CoreState, and the
+	// index of the last warrior to modify it, or -1 if no warrior has
+	// modified the address.
+	GetMemState(a Address) (CoreState, int)
+}
+
+type recorder struct {
+	sim      ReportingSimulator
+	coresize Address
+	color    []int
+	state    []CoreState
+}
+
+func NewRecorder(s ReportingSimulator) Recorder {
+	return newRecorder(s)
+}
+
+func newRecorder(sim ReportingSimulator) *recorder {
+	coresize := sim.CoreSize()
+
+	color := make([]int, coresize)
+	for i := Address(0); i < coresize; i++ {
+		color[i] = -1
+	}
+
+	state := make([]CoreState, coresize)
+
+	return &recorder{
+		sim:      sim,
+		coresize: coresize,
+		color:    color,
+		state:    state,
+	}
+}
+
+func (r *recorder) GetMemState(a Address) (CoreState, int) {
+	return r.state[a], r.color[a]
+}
+
+func (r *recorder) reset() {
+	for i := Address(0); i < r.coresize; i++ {
+		r.state[i] = CoreEmpty
+		r.color[i] = -1
+	}
+}
+
+func (r *recorder) Report(report Report) {
+	switch report.Type {
+	case SimReset:
+		r.reset()
+	case WarriorSpawn:
+		w := r.sim.GetWarrior(report.WarriorIndex)
+		for i := report.Address; i < report.Address+Address(w.Length()); i++ {
+			r.color[i%r.coresize] = report.WarriorIndex
+			r.state[i%r.coresize] = CoreCodeWritten
+			fmt.Println(i % r.coresize)
+		}
+	case WarriorTaskPop:
+		r.color[report.Address] = report.WarriorIndex
+		if r.sim.GetMem(report.Address).Op == DAT {
+			r.state[report.Address] = CoreDataExecuted
+		} else {
+			r.state[report.Address] = CoreCodeExecuted
+		}
+	case WarriorWrite:
+		r.color[report.Address] = report.WarriorIndex
+		if r.sim.GetMem(report.Address).Op == DAT {
+			r.state[report.Address] = CoreDataWritten
+		} else {
+			r.state[report.Address] = CoreCodeWritten
+		}
+	case WarriorRead:
+		r.color[report.Address] = report.WarriorIndex
+		r.state[report.Address] = CoreRead
+	case WarriorIncrement:
+		r.color[report.Address] = report.WarriorIndex
+		r.state[report.Address] = CoreIncremented
+	case WarriorDecrement:
+		r.color[report.Address] = report.WarriorIndex
+		r.state[report.Address] = CoreDecremented
+	}
+}
